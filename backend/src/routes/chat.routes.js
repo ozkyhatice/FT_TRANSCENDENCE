440eb92ecec22d7aae18a314688ce0ac7fd506c5
Hatice Özkaya <ozkyyhatice@gmail.com>
2025-07-01 14:34:55 +0300

    history endpoint defined

--- backend/src/routes/chat.routes.js
+++ backend/src/routes/chat.routes.js
@@ -1,4 +1,4 @@
-import { sendMessageController, getMessagesController } from '../controllers/chat.controller.js';
+import { sendMessageController, getMessagesController, getChatHistoryController } from '../controllers/chat.controller.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 import { postChatSchema } from '../schemas/chatSchemas.js';
 
@@ -11,5 +11,9 @@ export default async function chatRoutes(app, options) {
     app.get('/messages/:receiverId', {
         preHandler: verifyJWT,
         handler: getMessagesController
+    });
+    app.get('/history/:receiverId', {
+        preHandler: verifyJWT,
+        handler: getChatHistoryController
     });
 }
