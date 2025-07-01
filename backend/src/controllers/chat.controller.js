9bbf8c36220d53b78b31cb83daf10d50e069dc39
Hatice Özkaya <ozkyyhatice@gmail.com>
2025-07-01 14:36:12 +0300

    chat updated

--- backend/src/controllers/chat.controller.js
+++ backend/src/controllers/chat.controller.js
@@ -1,4 +1,5 @@
 import * as chatService from '../services/chat.service.js';
+import { broadcastMessage } from '../services/websocket.service.js';
 
 
 export async function sendMessageController(request, reply) {
@@ -16,6 +17,8 @@ export async function sendMessageController(request, reply) {
         }
         try{
         const message = await chatService.sendMessageService(senderID, receiverId, content);
+        broadcastMessage(parseInt(senderID), parseInt(receiverId), content, message.createdAt);
+        
         return reply.code(201).send({message});
         }catch(error){
             return reply.code(400).send({error: error.message});
@@ -37,5 +40,16 @@ export async function getMessagesController(request, reply) {
     }catch(error){
         console.error('Error getting messages:', error);
         return reply.code(500).send({error: 'Failed to get messages'});
+    }
+}
+
+export async function getChatHistoryController(request, reply) {
+    try{
+        const senderID = request.user.userId;
+        const receiverId = request.params.receiverId;
+        const chatHistory = await chatService.getChatHistoryService(senderID, receiverId);
+        return reply.code(200).send({chatHistory});
+    }catch(error){
+        return reply.code(400).send({error: error.message});
     }
 }
